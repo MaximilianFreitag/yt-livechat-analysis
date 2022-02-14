15630264577ccead7238ea57099376fde8393785
Maximilian Freitag <46624616+MaximilianFreitag@users.noreply.github.com>
2022-02-14 11:22:04 +0100

    Update example.py

--- example.py
+++ example.py
@@ -50,13 +50,12 @@ with col3:
     st.title('Youtube Livestream Analyse')
 
     st.write('Gebe eine URL eines bereits beendeten YouTube livestream und vergewissere dich, dass die Wiedergabe des chats aktiviert wurde. ')
-    st.write(' ')
     st.write('Die Analyse funktioniert nicht bei gerade laufenden livestreams! ')
-    st.write(' ')
+    
     st.write('Hier ist eine Beispiel URL eines streams der 60 Minuten ging. Ich habe leider keinen kürzeren stream gefunden...')
-    st.write(' ')
+    
     st.code('https://www.youtube.com/watch?v=e7EVbT0W9uU')
-    st.write(' ')
+    
     st.write('Wenn du deine URL eingibst und auf "Start" drückst, wird die Analyse gestartet. Die Analyse läuft solange bis der stream einmal komplett durchgelaufen ist. Sprich, wenn der stream 2 Stunden ging, muss man 2 Stunden auf das Ergebnis warten.')
     st.write(' ')
     
