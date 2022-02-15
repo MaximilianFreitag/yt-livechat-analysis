2b5c3899be4d62b05ad508cea70f16f7465eb0e3
Maximilian Freitag <46624616+MaximilianFreitag@users.noreply.github.com>
2022-02-15 19:15:22 +0100

    Update example.py

--- example.py
+++ example.py
@@ -89,7 +89,7 @@ with col3:
     #if url is empty display enter a url
     if url == '':
         st.write('Gebe die URL hier ein und drücke "Start"')
-        video_id = ' '
+        video_id = 'QH2-TGUlwu4'
 
     #if the url does not start with https://www.youtube.com/watch?v=
     if url.startswith('https://www.youtube.com/watch?v='):
