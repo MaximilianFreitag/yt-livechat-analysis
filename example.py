17ae7a85ce5195ff39877512e6d192a752f9debe
Maximilian Freitag <46624616+MaximilianFreitag@users.noreply.github.com>
2022-02-22 18:10:23 +0100

    Update example.py

--- example.py
+++ example.py
@@ -345,7 +345,7 @@ def main():
                 messages_lower.append(lowercase)
 
                 #IF YOU WANT TO SEE EACH MESSAGE REMOVE THE # IN THE NEXT LINE
-                #st.write(f" {c.author.name} // {c.message} // {c.elapsedTime} // {c.amountString}")
+                st.write(f" {c.author.name} // {c.message} // {c.elapsedTime} // {c.amountString}")
                 
 ##########################################################################################################################################
 
